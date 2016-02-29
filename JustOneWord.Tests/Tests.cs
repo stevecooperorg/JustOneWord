11d14eec0e5ef39d85ae764ee8e30a04d2351d89
Steve Cooper <steve@stevecooper.org>
2016-02-29 19:29:30 +0000

    inital commit

---
+++ JustOneWord.Tests/Tests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace JustOneWord.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void JustOneWord_Nevertheless_Neverland()
+        {
+            var dict = new HashSet<string>();
+            dict.Add("never");
+            dict.Add("land");
+            dict.Add("neverland");
+            dict.Add("the");
+            dict.Add("less");
+            dict.Add("nevertheless");
+           
+            var searcher = new Searcher();
+            var twoPartWords = searcher.words(2, dict, dict).ToArray();
+            Assert.AreEqual("-never-land-", twoPartWords[0]);
+
+            var threePartWords = searcher.words(3, dict, dict).ToArray();
+            Assert.AreEqual("-never-the-less-", threePartWords[0]);
+        }
+
+        [TestMethod]
+        public void JustOneWord_EntireDictionary()
+        {
+            var resourceFolder = @"C:\src\JustOneWord\JustOneWord\Resources";
+            var loader = new DictionaryLoader();
+            var candidates = loader.Load(resourceFolder, "english-words.*");
+            var officialWords = loader.Load(resourceFolder, "sowpods.txt");
+            officialWords.Add("A");
+
+            var searcher = new Searcher();
+
+            var all = new List<string>();
+            foreach (var word in searcher.words(3, candidates, officialWords))
+            {
+                all.Add(word);
+            }
+
+            all = all.OrderBy(s => -s.Length).ToList();
+
+            //*
+            var funny = new[] { "BUM", "POO", "WEE", "CUM" }
+                .Select(s => $"-{s}-")
+                .ToArray();
+
+            all.RemoveAll(s => !funny.Any(f => s.Contains(f)));
+            //*/
+            foreach (var word in all)
+            {
+                //word = word.Substring(2, word.Length - 2);
+                var inSowpods = officialWords.Contains(word.Replace("-", ""));
+                var display = inSowpods ? word.ToUpperInvariant() : word.ToLowerInvariant();
+                Console.WriteLine($"{display}  - {word.Length-4}");
+            }
+        }
+
+
+    }
+}
---
+++ JustOneWord/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Candidates = System.Collections.Generic.HashSet<string>;// System.Collections.Generic.Dictionary<string, string>;
+
+namespace JustOneWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wordlistPath = @"\\Mac\Home\Downloads\scowl-2016.01.19\final\english-words.95";
+            if (!File.Exists(wordlistPath))
+            {
+                throw new FileNotFoundException("no dictionary found", wordlistPath);
+            }
+
+
+        }
+    }
+
+    public class DictionaryLoader
+    {
+        public HashSet<string> Load(string wordlistPath, string pattern)
+        {
+            var items = new HashSet<string>();
+
+            foreach (var file in Directory.GetFiles(wordlistPath, pattern))
+            {
+                var content = File.ReadAllLines(file);
+
+                foreach (var word in content)
+                {
+                    items.Add(word.ToUpperInvariant());
+                }
+            }
+
+            return items;
+        }
+    }
+
+    public class Searcher
+    {
+        public IEnumerable<string> words(int numberOfWords, HashSet<string> candidates, HashSet<string> parentDictionary)
+        {
+            foreach (var word in candidates)
+            {
+                foreach (var result in wordsAtDepth(numberOfWords, "", word, parentDictionary))
+                {
+                    yield return result + "-";
+                }
+            }
+
+        }
+
+        public IEnumerable<string> wordsAtDepth(int depth, string previousPrefix, string remainder, HashSet<string> master)
+        {
+            if (depth == 1)
+            {
+                if (master.Contains(remainder))
+                {
+                    yield return previousPrefix + "-" + remainder;
+                }
+            }
+            else
+            {
+                for (var l = 1; l < remainder.Length - depth; l++)
+                {
+                    var prefix = remainder.Substring(0, l);
+                    var candidate = remainder.Substring(l);
+                    if (master.Contains(prefix))
+                    {
+                        var newPrefix = $"{previousPrefix}-{prefix}";
+                        foreach (var result in wordsAtDepth(depth - 1, newPrefix, candidate, master))
+                        {
+                            yield return result;
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+}
